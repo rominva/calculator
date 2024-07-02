5c87db315b7ef648d269f1436b895b1b88814854
Romina Valehi <valehi.romina@gmail.com>
2024-07-02 21:59:48 +0330

    Create caculator.py
    A simple calculator with python.

---
+++ caculator.py
@@ -0,0 +1,21 @@
+# "input" will get data from users
+# num1 & num2 are strings but they souldn't!
+# "int" or "float" to change it from a string to number
+
+while True:
+    num1 = float(input("\n💜 Enter your first number: "))
+    num2 = float(input("💛 Enter your second number: "))
+    oper = input("🧡 Enter the operation: ")
+    print("💚 resault:")
+
+    if oper == "+":
+        print(num1 + num2)
+    elif oper == "-":
+        print(num1 - num2)
+    elif oper == "*":
+        print(num1 * num2)
+    elif oper == "/":
+        print(num1 / num2)
+
+    else:
+        print("wrong data!")
